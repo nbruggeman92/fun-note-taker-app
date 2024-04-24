539a14e1a00d8444e5b054999a3766cb6c258e78
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-04-24 15:24:07 -0700

    more updates to server.js and store.js

--- db/store.js
+++ db/store.js
@@ -1,2 +1,44 @@
 const util = require("util");
-const fs = require ("fs");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync("db/db.json", "utf8")
+    }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note, null, 4))
+    }
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes))
+            } catch(error) {
+                parsedNotes = []
+            }
+            return parsedNotes;
+        })
+    }
+    addNotes(notes) {
+        const { title, text } = notes
+        if(!title || !text) {
+            throw new Error("Please fill in all fields")
+        }
+        const newNote = { title, text, id: uuidv4()}
+        return this.getNotes().then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote);
+    }
+    removeNotes(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes))
+    }
+}
+const a = new Store().getNotes().then((notes) => console.log(notes))
+
+module.exports = new Store();
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const store = require("./db/store");
+// possibly move this ^
 
 const app = express();
 
@@ -8,7 +10,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/notes", (req, res) => {
+    store.getNotes().then((notes) => {
+        return res.status(200).json(notes)
+    }).catch((error) => res.status(500).json(error))
     
+})
+
+app.post("/api/notes", (req, res) => {
+    store.addNotes(req.body).then((note) => {
+        return res.status(200).json(note)
+    }).catch((error) => res.status(500).json(error))
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    store.removeNotes(req.params.id).then(() => {
+        return res.status(200).json({ delete : true, id: req.params.id})
+    }).catch((error) => res.status(500).json(error))
 })
 
 app.listen(PORT, () => {
